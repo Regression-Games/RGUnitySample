e22d4aceb529e7f1579a4a9f794978f82ff7512a
vontell <vontell@mit.edu>
2023-08-21 15:23:38 -0400

    Add write permissions

--- Assets/Tests/RGBotTests.cs
+++ Assets/Tests/RGBotTests.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using RegressionGames;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.TestTools;
@@ -13,8 +16,21 @@ public class RGBotTests
         string rgApiKey = Environment.GetEnvironmentVariable("RG_API_KEY");
 
         yield return new WaitForFixedUpdate();
+        // Start RG
+        RGBotServerListener.GetInstance()?.StartGame();
         Assert.AreEqual(botId, "32");
         Assert.AreEqual(rgApiKey, "ABC");
+        // Start the bot
+        int[] botIds = {Int32.Parse(botId)};
+        int errorCount = 0;
+        Task.WhenAll(botIds.Select(botId =>
+            RGServiceManager.GetInstance()
+                ?.QueueInstantBot((long)botId, (botInstance) => { }, () => errorCount++)));
+        if (errorCount > 0)
+        {
+            Debug.Log($"Error starting {errorCount} of {botIds.Length} RG bots, starting without them");
+        }
+        // Let the test run
     }
     
 }
