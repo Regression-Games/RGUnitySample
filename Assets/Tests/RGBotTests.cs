9c309292b746e5204021247e15e7575887e70c5a
vontell <vontell@mit.edu>
2023-08-21 15:18:52 -0400

    Add write permissions

--- Assets/Tests/RGBotTests.cs
+++ Assets/Tests/RGBotTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -8,8 +9,12 @@ public class RGBotTests
     [UnityTest]
     public IEnumerator RunBotTest()
     {
+        string botId = Environment.GetEnvironmentVariable("RG_BOT");
+        string rgApiKey = Environment.GetEnvironmentVariable("RG_API_KEY");
+
         yield return new WaitForFixedUpdate();
-        Assert.AreEqual(1, 1);
+        Assert.AreEqual(botId, "32");
+        Assert.AreEqual(rgApiKey, "ABC");
     }
     
 }
