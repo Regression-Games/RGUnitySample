b1fe8cba8ec2c85ed8c7848db1ddda0d43473796
vontell <vontell@mit.edu>
2023-08-31 14:07:15 -0400

    Remove print statement on every loop

--- Assets/Tests/RGBotTests.cs
+++ Assets/Tests/RGBotTests.cs
@@ -81,7 +81,7 @@ public class RGBotTests
                 RGBotServerListener.GetInstance()?.StopGame();
                 Assert.Fail($"{DateTime.Now:yyyy-MM-dd- HH:mm:ss:ffff} Bots failed to connect within {TIMEOUT_IN_SECONDS} seconds");
             }
-            Debug.Log($"WAITING FOR BOTS TO CONNECT... inside the loop in thread {Thread.CurrentThread.ManagedThreadId}");
+            // Debug.Log($"WAITING FOR BOTS TO CONNECT... inside the loop in thread {Thread.CurrentThread.ManagedThreadId}");
             yield return null;
         }
         
